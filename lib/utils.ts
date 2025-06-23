9605f2624c8580c131c2938574394a92162872ec
Nethmi Anjani <anjaninethmi23@gmail.com>
2025-06-23 23:09:14 +0530

    auth updated and env creation

--- lib/utils.ts
+++ lib/utils.ts
@@ -203,7 +203,8 @@ export const authFormSchema = (type:string)=> z.object({
   //sign-up
   firstName: type==='sign-in'? z.string().optional() : z.string().min(3),
   lastName: type === 'sign-in' ? z.string().optional() : z.string().min(2),
-  address: type === 'sign-in' ? z.string().optional() : z.string().min(20),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().min(20),
+  city: type === 'sign-in' ? z.string().optional() : z.string().min(20),
   state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   ssn: type === 'sign-in' ? z.string().optional() : z.string().min(2),
